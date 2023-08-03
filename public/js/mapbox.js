413c794580ad86deb3f62cc53c1c499be22d58a5
Sunil Chauhan <sunil121ml@gmail.com>
2023-08-03 17:09:16 +0530

    finishing map integration

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,9 +2,45 @@ const locations = JSON.parse(document.getElementById("map").dataset.locations);
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYmFiYS1uYXN0eWMiLCJhIjoiY2xrcG43cHlwMDl0ajNncG5mem8zOHVreiJ9.aQNMeBNCf2yzg9hTBf6big";
+
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v12",
+  // scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  //creating marker
+  const el = document.createElement("div");
+  el.className = "marker";
+
+  //adding marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: "bottom",
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  //Add Popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  //extends map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
